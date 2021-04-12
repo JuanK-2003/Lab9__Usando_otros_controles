f4377f468a9901e2c41764713af1a1d8e6542267
Juan Carlos Neíl Palacios Escobar <67353415+JuanK-2003@users.noreply.github.com>
2021-04-11 23:53:55 -0600

    Add files via upload

---
+++ Clases/Alumno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exercise_of_inheritance.Clases
+{
+    public class Nota
+    {
+        public double calificacion { get; set; }
+
+        public string curso { get; set; }
+    }
+
+    public class Alumno : Datos
+    {
+        public string Carne { get; set; }
+        List<Nota> notas = new List<Nota>();
+
+        public List<Nota> Notas { get => notas; set => notas = value; }
+
+        public Alumno()
+        {
+            Notas = new List<Nota>();
+        }
+    }
+}
---
+++ Clases/Datos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exercise_of_inheritance.Clases
+{
+    public class Datos
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+
+        public Datos()
+        {
+        }
+
+        public Datos(string nombre, string apellido, string direccion, DateTime fechaNacimiento)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.direccion = direccion;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+
+        public int calcEdad()
+        {
+            DateTime fechaActual = DateTime.Today;
+            int edad = fechaActual.Year - fechaNacimiento.Year;
+            if ( fechaNacimiento.Month > fechaActual.Month )
+            {
+                --edad;
+            }
+            return edad;
+        }
+    }
+}
